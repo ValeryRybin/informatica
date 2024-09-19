30b8f3d226ffd0fccce7917d4993aab369a0f8ad
ValeryRybin <valery.rybin.spb@gmail.com>
2024-09-19 21:00:00 +0300

    changed

--- 3.py
+++ 3.py
@@ -1,5 +1,8 @@
+
 a=input().split()
+c=len(a)
+b=""
 for i in range(len(a)):
-    c=a[0:i]+a[i+1:len(a)]
-    if a[i] not in c: 
-        print(a[i])
+    if a.count(a[i]) == 1:
+        b=b+a[i]+" "
+print(b)
